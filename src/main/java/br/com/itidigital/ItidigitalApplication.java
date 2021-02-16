7e51154dca6f0465a7693be5f34c64d08f1f08c6
Ana413 <ana_lucia.silva@hotmail.com>
2021-02-16 13:48:54 -0300

    Add files via upload

---
+++ src/main/java/br/com/itidigital/ItidigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.itidigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItidigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItidigitalApplication.class, args);
+	}
+
+}
